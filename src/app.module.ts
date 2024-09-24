832103bb585ef64273a6384a47bb73f648c535f7
RPaezA <136133391+RPaezA@users.noreply.github.com>
2024-09-24 15:14:32 -0500

    relaciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,13 @@ import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from './config/config.module';
 import { CategoriaModule } from './modules/categoria/categoria.module';
 import { PersonaModule } from './modules/persona/persona.module';
+import { ProductoModule } from './modules/producto/producto.module';
+import { RolModule } from './modules/rol/rol.module';
+import { ClienteModule } from './modules/cliente/cliente.module';
+import { PedidoModule } from './modules/pedido/pedido.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, DatabaseModule, ConfigModule, CategoriaModule, PersonaModule],
+  imports: [AuthModule, UsersModule, DatabaseModule, ConfigModule, CategoriaModule, PersonaModule, ProductoModule, RolModule, ClienteModule, PedidoModule],
   controllers: [AppController],
   providers: [AppService],
 })
